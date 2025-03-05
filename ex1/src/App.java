afa3828788eaecc477c87f5521600e0391840909
clark-ti-mg <153373604+clark-ti-mg@users.noreply.github.com>
2025-03-05 18:49:28 -0300

    Update App.java

--- ex1/src/App.java
+++ ex1/src/App.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 
 public class App {
-    
+
     public static void main(String[] args) throws Exception {
         Scanner teclado = new Scanner(System.in);
         int matricula;
         String nome, curso;
 
         BSTree bst = new BSTree();
-        
+
         int tam = 0;
 
         try {
-            while(tam < 10){
+            while (tam < 10) {
                 System.out.println("--------------------------------------------");
                 System.out.println("Aluno " + tam);
 
                 System.out.println("Nome do aluno:");
                 nome = teclado.nextLine();
-    
+
                 System.out.println("Curso do aluno:");
-                curso =  teclado.nextLine();
-              
+                curso = teclado.nextLine();
+
                 System.out.println("Matrícula: ");
                 matricula = teclado.nextInt();
 
@@ -34,11 +34,27 @@ public class App {
         } catch (Exception e) {
             System.out.println("Exceção ao capturar os dados: " + e.getMessage());
         }
-        
+
         try {
             bst.preOrdem(bst.raiz);
         } catch (Exception e) {
             System.out.println("Exceção ao exibir os dados: " + e.getMessage());
+        }
+
+        System.out.println("Aluno com maior matrícula na árvore: ");
+        System.out.println(bst.maximo(bst.raiz).aluno.matricula);
+
+        System.out.println("Qual matrícula quer achar na árvore?");
+        try {
+            int matriculaBusca = teclado.nextInt();
+
+            if (bst.encontreNaArvore(matriculaBusca)) {
+                System.out.println("Aluno cadastrado.");
+            } else {
+                System.out.println("Aluno não cadastrado.");
+            }
+        } catch (Exception e) {
+            System.out.println("Exceção ao capturar matrícula: " + e.getMessage());
         }
 
     }
